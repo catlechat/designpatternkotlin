f0fd61148ddb43c157604dacefbb188275a6efc2
dkaminowicz <dankamino@hotmail.fr>
2022-06-26 22:40:36 +0200

    add class

---
+++ app/src/main/java/com/ivan/ceaicovschi/tarotdesignpattern/SaveScore.kt
@@ -0,0 +1,10 @@
+package com.ivan.ceaicovschi.tarotdesignpattern
+
+class SaveScore {
+     var player1: String = ""
+    var player2: String = ""
+    var player3: String = ""
+    var player4: String = ""
+    var horizontal_line: String = ""
+    var date: String = ""
+}
