90c3e07830ce7d6cf46904c9c156a90a03cfcb47
dkaminowicz <dankamino@hotmail.fr>
2022-06-26 22:29:29 +0200

    view save score

---
+++ app/src/main/java/com/ivan/ceaicovschi/tarotdesignpattern/SaveScoreActivity.kt
@@ -0,0 +1,11 @@
+package com.ivan.ceaicovschi.tarotdesignpattern
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SaveScoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_save_score)
+    }
+}
